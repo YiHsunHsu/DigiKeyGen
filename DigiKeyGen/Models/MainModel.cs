512d912bb0ba9ee1e584eb38cd00ff1ba87b66c8
Eason_Hsu <hsunslife@gmail.com>
2017-06-02 10:28:30 +0800

    加入專案檔案。

---
+++ DigiKeyGen/Models/MainModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DigiKeyGen.Models
+{
+    public class MainModel
+    {
+        /// <summary>
+        /// 醫事機構代碼
+        /// </summary>
+        public string ClinicCode { get; set; }        
+    }
+}
---
+++ DigiKeyGen/ViewModels/MainViewModel.cs
@@ -0,0 +1,69 @@
+﻿using DigiKeyGen.Models;
+using DigiKeyGen.ViewModels.Class;
+using DigiKeyGen.ViewModels.Commands;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DigiKeyGen.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private MainModel mainModel;
+        private KeyGenerator keyGenerator;
+
+        public MainViewModel()
+        {
+            mainModel = new MainModel();
+            keyGenerator = new KeyGenerator();
+        }
+
+        public string ClinicCode
+        {
+            get { return mainModel.ClinicCode; }
+            set
+            {
+                mainModel.ClinicCode = value;
+                OnPropertyChanged("ClinicCode");
+                OnPropertyChanged("ClincCodeEncode");
+            }
+        }
+        public string ClincCodeEncode
+        {
+            get { return string.IsNullOrEmpty(mainModel.ClinicCode) ? string.Empty : keyGenerator.SHA384Encode(mainModel.ClinicCode); }
+        }
+
+        private RelayCommand _copyKey;
+        public ICommand CopyKeyToClipboard
+        {
+            get
+            {
+                if (_copyKey == null)
+                {
+                    _copyKey = new RelayCommand(CopyKey, CanCopyKey);
+                }
+                return _copyKey;
+            }
+        }
+
+        private void CopyKey()
+        {
+            Clipboard.SetText(ClincCodeEncode);
+        }
+
+        private bool CanCopyKey()
+        {
+            if (string.IsNullOrEmpty(ClincCodeEncode))
+                return false;
+            else
+                return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
